aa8274186c4a9ed908ccd57f71dba3ee1f090e26
kenneth pegueros <kenneth_183@hotmail.com>
2023-05-01 16:40:20 -0600

    Initial commit

---
+++ src/main/java/com/mycompany/llamadasafuncionej1/LlamadasAfuncionEj1.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.llamadasafuncionej1;
+
+/**
+ *
+ * @author kenne
+ */
+public class LlamadasAfuncionEj1 {
+
+   public void multiplicacion(int a, int b) {
+        int resultado = a*b;
+        System.out.println("el resultado =" + resultado );
+    }
+
+    public static void main(String[] args) {
+        LlamadasAfuncionEj1 op = new LlamadasAfuncionEj1();
+        op.multiplicacion(3, 2);
+    }
+}
